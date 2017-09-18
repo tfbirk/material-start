7a576c2e9bba5e88592b7e88962e7d8cc28ae394
taylor-fra-brown <taylor.fra.brown@hpe.com>
2017-09-18 13:19:28 -0500

    add the icon provider

--- app/src/app.js
+++ app/src/app.js
@@ -8,8 +8,12 @@ import 'angular-material';
 import AppController from 'src/AppController';
 import Users from 'src/users/Users';
 
-export default angular.module('starter-app, ['ngMaterial', Users.name])
-    .run(() => {
-        console.log(`Starting the 'starter-app' module`);
+export default angular.module('starter-app, [ 'ngMaterial', Users.name ] )
+    .config(($mdIconProvider) => {
+        // Register the user avatar icons
+        $mdIconProvider
+        .defaultIconSet("./assets/svg/avatars.svg", 128)
+        .icon("menu", "./assets/svg/menu.svg", 24)
+        .icon("share", "./assets/svg/share.svg", 24);
     })
     .controller('AppController', AppController);
